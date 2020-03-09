fd082c1c7287c59cca5795766fde1c5da88221c4
user <progexplorer@gmail.com>
2020-03-09 10:43:07 +0100

    chore(gatsby-node): createPages from mdx nodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,3 +12,55 @@ exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
     fs: "empty",
   }
 }
+
+const { createFilePath } = require("gatsby-source-filesystem")
+const path = require("path")
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve("src/templates/blogPostTemplate.tsx")
+
+  return graphql(`
+    {
+      allMdx {
+        nodes {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    const posts = result.data.allMdx.nodes
+
+    // create page for each mdx file
+    posts.forEach((post, index) => {
+      createPage({
+        path: post.fields.slug,
+        component: blogPostTemplate,
+        context: {
+          slug: post.fields.slug,
+        },
+      })
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `Mdx`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
