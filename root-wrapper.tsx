f32ee7d803e292859540b65ee464b4a4b7f63a0e
user <progexplorer@gmail.com>
2020-03-09 10:25:01 +0100

    feat(root-wrapper): add mdx provider to root element

--- root-wrapper.tsx
+++ root-wrapper.tsx
@@ -1,12 +1,13 @@
 import React, { ReactNode } from "react"
 import { ThemeProvider } from "emotion-theming"
+import { MDXProvider } from "@mdx-js/react"
 import { GlobalStyles, theme } from "./src/utils/global-styles"
 
 export const wrapRootElement = ({ element }): ReactNode => (
   <>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      {element}
+      <MDXProvider>{element}</MDXProvider>
     </ThemeProvider>
   </>
 )
